b37cc2274cd1f97f6e5ff33287fc3292d815414e
Yashas <yashaspancham@gmail.com>
2025-08-12 07:50:27 +0530

    Yashas-feat-AllPageSetUpDone

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,19 @@
-
-
+"use client";
 import React from "react";
-import { journalEntries } from "@/temp/tempData";
-import EntryCard from "@/components/EntryCard";
-import SideBarMenu from "@/components/SideBarMenu";
-import AddEntryButton from "@/components/addEntryButton";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router=useRouter();
   return (
-    <main className="w-full h-full z-0 lg:flex">
-      <SideBarMenu />
-      <div className="z-1 default flex flex-col h-full w-full items-center justify-center xl:p-20 lg:p-16 md:p-12 sm:p-8">
-        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 md:gap-8 place-items-center">
-          {journalEntries.map((item, index) => (
-            <div key={index}>
-              <EntryCard entry={item} />
-            </div>
-          ))}
-        </div>
+    <main className="w-full h-[100vh] flex items-center justify-center">
+      <div className="flex flex-col items-center gap-5">
+        <p className="text-4xl">Welcome to Reflections</p>
+        <button 
+        onClick={()=>router.push("/sign-in")}
+        className="bg-blue-500 p-2 w-40 rounded-lg text-md hover:bg-blue-600 hover:cursor-pointer text-white">
+          Sign in
+        </button>
       </div>
-      <AddEntryButton />
     </main>
   );
 }
--- components/SideBarMenu/index.tsx
+++ components/SideBarMenu/index.tsx
@@ -1,42 +1,90 @@
 "use client";
 
 import React from "react";
-import { usePathname } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import Image from "next/image";
 
 const SideBarMenu = () => {
   const pathname = usePathname();
+  const router = useRouter();
+
   const pages = [
     {
-      logoSrc: "/icons/profileIcon.png",
-      path: "/profile",
-      pageName: "Profile",
-      altText:"Profile Icon"
+      logoSrc: "/icons/entryIcon.png",
+      path: "/entries",
+      pageName: "Entries",
+      altText: "Entries Icon",
     },
-    { logoSrc: "/icons/entryIcon.png", path: "/", pageName: "Entries",altText:"Entries Icon" },
-    { logoSrc: "/icons/mediaIcon.png", path: "/media", pageName: "Media",altText:"Media Icon" },
+    {
+      logoSrc: "/icons/mediaIcon.png",
+      path: "/media",
+      pageName: "Media",
+      altText: "Media Icon",
+    },
+    {
+      logoSrc: "/icons/tasksIcon.png",
+      path: "/tasks",
+      pageName: "Tasks",
+      altText: "Tasks Icon",
+    },
+    {
+      logoSrc: "/icons/calendarIcon.png",
+      path: "/calender",
+      pageName: "Calender",
+      altText: "Calender Icon",
+    },
   ];
 
+  const handleRoutes = (currentPath: string, newPath: string) => {
+    if (currentPath !== newPath) {
+      router.push(newPath);
+    }
+  };
+
   return (
-    <div className="fixed lg:sticky z-10 left-0 bottom-0 lg:top-0 bg-blue-800 h-20 min-w-full lg:min-w-[200px] lg:h-[100vh]">
-      <div className="flex lg:pt-20 lg:flex-col max-lg:gap-10 sm:gap-5 lg:gap-0 items-center max-lg:justify-center">
+    <div className="flex max-lg:flex-wrap max-lg:gap-2 lg:flex-col lg:justify-between fixed lg:sticky z-10 left-0 bottom-0 lg:top-0 bg-blue-800 min-h-20 min-w-full lg:min-w-[200px] lg:h-[100vh]">
+      
+      {/* Main menu group */}
+      <div className="flex lg:pt-20 lg:flex-col max-lg:flex-wrap max-lg:gap-1 sm:gap-5 lg:gap-0 items-center max-lg:justify-center flex-1">
         {pages.map((page, index) => (
           <button
+            onClick={() => handleRoutes(pathname, page.path)}
             key={index}
-            className={`flex items-center justify-center gap-5 lg:w-[100%] hover:cursor-pointer max-lg:mt-5 max-lg:p-3 lg:py-5 rounded-md text-md sm:text-lg lg:text-2xl text-white max-lg:bg-blue-900 hover:bg-blue-700 
-                ${pathname === page.path ? "bg-blue-700" : ""}`}
+            className={`flex items-center justify-between xl:px-5 gap-5 lg:w-full hover:cursor-pointer max-lg:mt-2 max-lg:p-3 lg:py-5 rounded-md text-md sm:text-lg lg:text-2xl text-white max-lg:bg-blue-900 hover:bg-blue-700 ${
+              pathname === page.path ? "bg-blue-700" : ""
+            }`}
           >
             <Image
               src={page.logoSrc}
               alt={page.altText}
-              width="40"
-              height="40"
-              className="max-sm:hidden"
+              width={40}
+              height={40}
+              className="max-md:hidden"
             />
             <div>{page.pageName}</div>
           </button>
         ))}
       </div>
+
+      {/* Profile group */}
+      <div className="flex max-lg:justify-center lg:w-full">
+        <button
+          onClick={() => handleRoutes(pathname, "/profile")}
+          className={`flex items-center justify-center gap-5 lg:w-full hover:cursor-pointer max-lg:mt-2 max-lg:p-3 lg:py-5 rounded-md text-md sm:text-lg lg:text-2xl text-white max-lg:bg-blue-900 hover:bg-blue-700 ${
+            pathname === "/profile" ? "bg-blue-700" : ""
+          }`}
+        >
+          <Image
+            src="/icons/profileIcon.png"
+            alt="Profile Icon"
+            width={40}
+            height={40}
+            className="max-md:hidden"
+          />
+          <div>Profile</div>
+        </button>
+      </div>
+
     </div>
   );
 };
