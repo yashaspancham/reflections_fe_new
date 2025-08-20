3896e60df2391b1e173b313ac986c915095188a8
Yashas <yashaspancham@gmail.com>
2025-08-20 21:01:57 +0530

    Yashas-feat-TaskUIDOne

--- components/addTaskButton/index.tsx
+++ components/addTaskButton/index.tsx
@@ -3,11 +3,11 @@
 import { usePathname } from "next/navigation";
 import { getColor } from "@/utils/getColorForSideMenu";
 
-const AddTaskButton = () => {
+const AddTaskButton = ({setSideMenuBool}:any) => {
   const pathname = usePathname();
   return (
     <button
-      onClick={() => {}}
+      onClick={() => {setSideMenuBool(true)}}
       className={`z-10 fixed right-5 lg:right-20 bottom-36 sm:bottom-24 lg:bottom-10 ${getColor(pathname)[1]} py-1 px-2 lg:px-3 lg:py-2 rounded-xl hover:cursor-pointer ${getColor(pathname)[2]}`}
     >
       <p className="text-white text-5xl">+</p>
