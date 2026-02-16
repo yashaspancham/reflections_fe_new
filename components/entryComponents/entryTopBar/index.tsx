446f9edc0624e059bd8fcc3fcfab6ef2595c080c
Yashas <yashaspancham@gmail.com>
2026-02-16 22:25:53 +0530

    Yashas-feat-DisableSubmitAuthPages

--- components/entryComponents/entryTopBar/index.tsx
+++ components/entryComponents/entryTopBar/index.tsx
@@ -33,8 +33,6 @@ const EntryTopBar = ({
       setEditorTick((t) => t + 1);
     };
 
-    // subscribe to editor updat
-    // es and selection changes so toolbar stays in sync
     editor.on("update", handle);
     editor.on("selectionUpdate", handle);
     editor.on("transaction", handle);
